bb67ecb5ff293838b651328e43623645c811a676
haokairui <haokairui@hualala.com>
2019-03-02 21:14:31 +0800

    测试代码

--- app/src/main/java/com/example/hll_mall/myaidldemoserver/MyService.java
+++ app/src/main/java/com/example/hll_mall/myaidldemoserver/MyService.java
@@ -3,13 +3,23 @@ package com.example.hll_mall.myaidldemoserver;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class MyService extends Service {
     IMyAidlInterface.Stub mStub = new IMyAidlInterface.Stub() {
+
+        @Override
+        public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            Log.d("测试", "onTransact");
+            return super.onTransact(code, data, reply, flags);
+        }
+
         @Override
         public int add(int arg1, int arg2) throws RemoteException {
+            Log.d("测试", "add");
             return arg1 + arg2;
         }
     };
